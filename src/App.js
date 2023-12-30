4295dfa742e85d1aeb69a9c6c3d327dc7125c8e1
hassan abass <abass.hassan.7798@gmail.com>
2023-12-30 01:50:07 +0100

    Adding GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Home from "./component/Home";
 import CountriesDetails from "./component/CountriesDetails";
@@ -6,13 +6,15 @@ function App() {
   return (
     <div className="App">
       <div className="Navigation-section">Where in the world?</div>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route
-          path="/country/:population"
-          element={<CountriesDetails />}
-        ></Route>
-      </Routes>
+      <HashRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/country/:population"
+            element={<CountriesDetails />}
+          ></Route>
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
